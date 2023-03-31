9a46dfc91ad99692142822ce7785848096e18a6f
David F Bachor <davidfbachor@gmail.com>
2023-03-30 21:44:58 -0400

    Add files via upload

---
+++ ServiceBusCore/Integration/IMessageBus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusCore.Integration
+{
+    public interface IMessageBus
+    {
+        Task PublishMessage(IntegrationBaseMessage message, string topicName);
+    }
+}
---
+++ ServiceBusCore/Integration/IntegrationBaseMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusCore.Integration
+{
+    public class IntegrationBaseMessage
+    {
+        public Guid Id { get; set; }
+        public DateTime CreationDateTime { get; set; }
+    }
+}
